9abe92bc100c6aead74c1ec32d09d2cde89395a2
Freek <freekpst@gmail.com>
2017-05-10 10:11:58 +0200

    Initial setup of trade bot

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,7 @@
 const Bot = require('./lib/Bot')
 const SOFA = require('sofa-js')
 const Fiat = require('./lib/Fiat')
+const request = require('request');
 
 let bot = new Bot()
 
@@ -27,20 +28,32 @@ bot.onEvent = function(session, message) {
 }
 
 function onMessage(session, message) {
-  welcome(session)
+  const isTradeInProgress = (session.get('tradeInProgress') || false)
+  if(isTradeInProgress) {
+    console.log('TRADE IS IN PROGRESS MESSAGE');
+    processTradeParameters(session, message);
+  } else {
+    welcome(session)
+  }
 }
 
 function onCommand(session, command) {
   switch (command.content.value) {
-    case 'ping':
-      pong(session)
-      break
-    case 'count':
-      count(session)
-      break
-    case 'donate':
-      donate(session)
-      break
+    case 'newTrade': 
+      startTrade(session);
+      break;
+    case 'cancelTrade':
+      cancelTrade(session);
+      break;
+    case 'acceptTrade':
+      acceptTrade(session);
+      break;
+    case 'rejectTrade':
+      rejectTrade(session);
+      break;
+    case 'testDM':
+      testDM(session);
+      break;
     }
 }
 
@@ -51,11 +64,169 @@ function onPayment(session) {
 // STATES
 
 function welcome(session) {
-  sendMessage(session, `Hello Token!`)
+  getUserName(session.address, function(userName) {
+    sendDefaultMessage(session, `Hello ${userName}!`)
+  });
+}
+
+function incorrectParameters(session) {
+  sendRequestTradeParameters(session, `Incorrect parameters.
+  Please enter the following trade details in a single message separated by comma:
+  username of trader (no @),
+  name of product to trade,
+  amount of product to trade,
+  amount of US dollars to trade for`);
+}
+
+function startTrade(session) {
+  session.set('tradeInProgress', true);
+  sendRequestTradeParameters(session, `Please enter the following trade details in a single message separated by comma:
+  username of trader (no @),
+  name of product to trade,
+  amount of product to trade,
+  amount of US dollars to trade for`);
+}
+
+function cancelTrade(session) {
+  session.set('tradeInProgress', false);  
+  sendDefaultMessage(session, `Trade canceled`);
+}
+
+function acceptTrade(session) {
+  session.set('tradeInProgress', false);
+  const message = 'Trade accepted, adding to smart contract and forwarding to back office';
+  sendDefaultMessage(session, message);
+  const traderAddress = session.get('tradingWith');
+  sendDefaultDMMessage(traderAddress, message);
+}
+
+function rejectTrade(session) {
+  session.set('tradeInProgress', false);  
+  sendDefaultMessage(session, `Trade rejected`);
+  const traderAddress = session.get('tradingWith');
+  sendDefaultDMMessage(traderAddress, 'Trade rejected');
+}
+
+function processTradeParameters(session, message) {
+  //sendDefaultMessage(session, message);
+  const parameters = message.body.split(',');
+  console.log('PARAMETERS', parameters);  
+  if(parameters.length == 4) {
+    getUserAddress(parameters[0], function(userAddress) {
+      console.log('USER ADDRESS FOUND', userAddress);
+      session.set('tradingWith', userAddress);
+      sendRequestTradeConfirmation(userAddress, `${parameters[0]} requested a trade of${parameters[2]} units of${parameters[1]} for${parameters[3]} dollars. Please accept or reject.`, session)
+    });
+  }
+  else {
+    welcome(session);
+  }
+}
+
+
+
+// UTILITY
+
+function getUserAddress(username, callback) {
+  request(`https://identity.service.tokenbrowser.com/v1/user/${username}`, (error, response, body) => {
+    console.log
+    const userInfo = JSON.parse(body);
+    callback(userInfo. token_id);
+  });
+}
+
+function getUserName(address, callback) {
+  request(`https://identity.service.tokenbrowser.com/v1/user/${address}`, (error, response, body) => {
+    const userInfo = JSON.parse(body);
+    console.log('USERINFO', userInfo);
+    console.log(userInfo.username);
+    callback(userInfo.username);
+  });
+}
+
+// HELPERS
+
+function sendDefaultMessage(session, message) {
+  let controls = [
+    {type: 'button', label: 'Create new trade', value: 'newTrade'},
+  ]
+  session.reply(SOFA.Message({
+    body: message,
+    controls: controls,
+    showKeyboard: false,
+  }))
+}
+
+function sendDefaultDMMessage(address, message) {
+  let controls = [
+    {type: 'button', label: 'Create new trade', value: 'newTrade'},
+  ]
+  bot.client.send(address, SOFA.Message({
+    body: message,
+    controls: controls,
+    showKeyboard: false,
+  }))
+}
+
+function sendRequestTradeParameters(session, message) {
+  let controls = [
+    {type: 'button', label: 'Cancel', value: 'cancelTrade'},
+  ]
+  session.reply(SOFA.Message({
+    body: message,
+    controls: controls,
+    showKeyboard: false,
+  }))
+}
+
+// Request trade confirmation from trader B
+function sendRequestTradeConfirmation(address, message, session) {
+  if(address === undefined) {
+    sendDefaultMessage(session, 'hi');
+    return;
+  }
+  
+  let controls = [
+    {type: 'button', label: 'Accept', value: 'acceptTrade'},
+    {type: 'button', label: 'Reject', value: 'rejectTrade'},
+  ]
+  bot.client.send(address, SOFA.Message({
+    body: message,
+    controls: controls,
+    showKeyboard: false,
+  }))
+}
+
+function sendTradeMessage(session, message) {
+  let controls = [
+    {type: 'button', label: 'Accept', value: 'accept'},
+    {type: 'button', label: 'Reject', value: 'reject'},
+  ]
+  session.reply(SOFA.Message({
+    body: message,
+    controls: controls,
+    showKeyboard: false,
+  }))
+}
+
+//OLD
+/*
+
+function sendMessage(session, message) {
+  let controls = [
+    {type: 'button', label: 'Ping', value: 'ping'},
+    {type: 'button', label: 'Count', value: 'count'},
+    {type: 'button', label: 'Donate', value: 'donate'}
+  ]
+  session.reply(SOFA.Message({
+    body: message,
+    controls: controls,
+    showKeyboard: false,
+  }))
 }
 
 function pong(session) {
-  sendMessage(session, `Pong`)
+  sendTradeMessage(session, `Pong`)
 }
 
 // example of how to store state on each user
@@ -71,18 +242,4 @@ function donate(session) {
     session.requestEth(toEth.USD(1))
   })
 }
-
-// HELPERS
-
-function sendMessage(session, message) {
-  let controls = [
-    {type: 'button', label: 'Ping', value: 'ping'},
-    {type: 'button', label: 'Count', value: 'count'},
-    {type: 'button', label: 'Donate', value: 'donate'}
-  ]
-  session.reply(SOFA.Message({
-    body: message,
-    controls: controls,
-    showKeyboard: false,
-  }))
-}
+*/
